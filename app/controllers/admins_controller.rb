2801849dbf0cc5a15ac1f9206ead6885ae806beb
jancnzhangmin <jancnzhangmin@hotmail.com>
2017-02-06 02:51:18 +0800

    selectdata

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,8 +4,11 @@ class AdminsController < ApplicationController
 
   end
 
-  def getdata
-    @admins=Admin.all
+  def selectdata
+    @admins=Admin.page(params[:pageNumber]).per(params[:pageSize])
+    #render json:@admins
+    render json: '{"total":' + Admin.count.to_s + ',"rows":' + @admins.to_json + '}'
+    #render jso: '{"total":5000,"rows":' + @admins.to_json + '"}'
   end
 
 end
